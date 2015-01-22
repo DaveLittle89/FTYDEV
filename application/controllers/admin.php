ea8c93ad6bc2e9500b24914c5438d6b6cc7a80cc
David Little <da.little@gmail.com>
2015-01-22 02:22:58 +0000

    New Update

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -6,6 +6,8 @@ class Admin extends CI_Controller {
 	{
 		parent::__construct();
 		
+		// Load Admin Function Model
+		$this->load->model('Admins');
 	}
 	
 	public function index()
@@ -13,6 +15,45 @@ class Admin extends CI_Controller {
 		
 	}
 	
+	public function login(){
+
+		
+		if($this->input->post('email')){
+			// Check Login
+
+				$admin = $this->admins->get_admin($this->input->post('email'));
+				
+				if($user){
+					if($admin['password'] == $this->general->passEnc($this->input->post('password'))){
+						// Log User In
+						$this->session->set_userdata('adminID', $admin['userID']);
+						// Redurect To Partner Page
+						if($this->session->userdata('package')){
+							redirect('package/book');
+						}else{
+							redirect('package/index');
+						}
+					}else{
+						$msg = "Incorrect Login";
+					}
+				}else{
+					$msg = "Incorrect Login";
+				}
+			
+			}
+			
+
+			$this->load->view('public_header');
+			$this->load->view('login', array('msg' => $msg));
+			$this->load->view('public_footer');			
+		
+	}
+
+
+	public function dashboard()
+	{
+		
+	}
 }
 
 ?>
--- application/models/admins.php
+++ application/models/admins.php
@@ -15,6 +15,14 @@ class Admins extends CI_Model
 		parent::__construct();
 	}
 	
+	function get_admin($email){
+		$this->db->from('admins');
+		$this->db->where(array('email' => $email));
+		$query = $this->db->get(); 
+		return ($this->db->count_all_results() == 0)? false:  $query->row_array();
+	}
+		
+	
 	function add_category($data){
 		$this->db->insert('categories', $data); 
 	}
